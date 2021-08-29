95f8067181b62ef0e387614dbee0e7f4169dcc9c
JukaChu <olexiy.yankevich@gmail.com>
2021-08-30 00:58:22 +0300

    account page

--- dist/js/scripts.min.js
+++ dist/js/scripts.min.js
@@ -31,3 +31,57 @@ $(document).ready(function () {
         sortField: 'text'
     });
 });
+
+// $(function() {
+//     $('input[name="daterange"]').daterangepicker({
+//         opens: 'left',
+//         locale: {
+//             format: 'DD/MM'
+//         }
+//     })
+// });
+let configObject = {
+    language: 'auto',
+    autoClose: true,
+    showShortcuts: false,
+    singleMonth: true,
+    showTopbar: false,
+    format: 'DD.MM.YYYY',
+    container: '.acc-date__container',
+    customOpenAnimation: function(cb)
+    {
+        $(this).fadeIn(300, cb);
+    },
+    customCloseAnimation: function(cb)
+    {
+        $(this).fadeOut(300, cb);
+    },
+    // setValue: function(s)
+    // {
+    //     if(!$(this).attr('readonly') && !$(this).is(':disabled') && s != $(this).val())
+    //     {
+    //         console.log($(this).val(s));
+    //         $(this).val(s);
+    //     }
+    // },
+};
+$('.acc-date-picker input').dateRangePicker(configObject)
+    .bind('datepicker-change',function(event,obj)
+    {
+        /* This event will be triggered when second date is selected */
+
+        let firstDay = obj.date1;
+        let secondDay = obj.date2;
+        let firstDayYear = firstDay.getFullYear();
+        let firstDayMonth = firstDay.getMonth() + 1;
+        let firstDayDay = firstDay.getDate();
+        let secondDayYear = secondDay.getFullYear();
+        let secondDayMonth = secondDay.getMonth() + 1;
+        let secondDayDay = secondDay.getDate();
+
+        console.log(obj);
+        console.log('First day:  ' + firstDayYear + '.'+ firstDayMonth + '.' + firstDayDay);
+        console.log('Last day:  ' + secondDayYear + '.'+ secondDayMonth + '.' + secondDayDay);
+
+
+    });
